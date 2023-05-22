0cedacd943536310830b9e8580d48273cf5f4ac8
ajgeiss0702 <ajgeiss72@gmail.com>
2023-05-22 10:21:48 -0700

    Add padding to contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,25 +1,31 @@
-<div class="limit mx-auto">
+<div class="limit mx-auto p-2">
     <h1 class="h1">Contact Me</h1>
 
     <h2 class="h2">Discord</h2>
 
-    If you already share a discord server with me, the best way to reach me is on discord.
-    <br>
-    <b class="numbers">I do not accept friend requests!</b>
-    <br>
-    My discord is <span class="numbers">ajgeiss0702#0702</span>
+    <p>
+        If you already share a discord server with me, the best way to reach me is on discord.
+        <br>
+        <b>I do not accept friend requests!</b>
+        <br>
+        My discord is <span class="numbers">ajgeiss0702#0702</span>
+    </p>
     <br>
 
     <h2 class="h2">Email</h2>
 
-    The second best way to contact me is via email.<br>
-    My main email address is
-    <span class="numbers">
-        <a href="mailto:ajgeiss0702@ajg0702.us">ajgeiss0702@ajg0702.us</a>
-    </span>
-    <br>
-    However, sometimes my mail server is kinda dumb, so I'd recommend CC'ing
-    <span class="numbers">
-        ajgeiss72@gmail.com
-    </span>
+    <p>
+        The second best way to contact me is via email.<br>
+        My main email address is
+        <a href="mailto:ajgeiss0702@ajg0702.us" class="font-bold">ajgeiss0702@ajg0702.us</a>
+        <br>
+        However, sometimes my mail server is kinda dumb, so I'd recommend CC'ing
+        <b>ajgeiss72@gmail.com</b>
+    </p>
 </div>
+
+<style>
+    p {
+        padding-left: 0.25rem;
+    }
+</style>

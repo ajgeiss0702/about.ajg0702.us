68a0cdfcf1f5f5d0085dcbc5f3e589191be4da90
ajgeiss0702 <ajgeiss72@gmail.com>
2023-09-27 11:23:40 -0700

    period lol

--- src/routes/deckard/+page.svelte
+++ src/routes/deckard/+page.svelte
@@ -1,5 +1,5 @@
 <title>Introducing Valve Zen</title>
-<meta name="description" content="Introducing Valve Zen. Valve's next-generation standalone high fidelity virtual reality device">
+<meta name="description" content="Introducing Valve Zen. Valve's next-generation standalone high fidelity virtual reality device.">
 <meta property="og:image" content="https://store.akamai.steamstatic.com/public/shared/images/responsive/share_steam_logo.png"/>
 <br>
 <br>

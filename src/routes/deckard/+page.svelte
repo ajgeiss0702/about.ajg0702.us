80dd9221e749571ba5979e40997e77ac2cf13024
ajgeiss0702 <ajgeiss72@gmail.com>
2023-09-27 11:20:21 -0700

    lol

---
+++ src/routes/deckard/+page.svelte
@@ -0,0 +1,8 @@
+<title>Introducing Valve Zen</title>
+<meta name="description" content="Introducing Valve Zen. Valve's next-generation standalone high fidelity virtual reality device">
+<meta property="og:image" content="https://store.akamai.steamstatic.com/public/shared/images/responsive/share_steam_logo.png"/>
+<br>
+<br>
+<div class="text-center">
+    <h1 class="h1">Trolled lol</h1>
+</div>

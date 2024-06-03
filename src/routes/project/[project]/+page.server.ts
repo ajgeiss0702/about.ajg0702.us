5275eee814a1682f4acd3a0020e2241735f1e934
ajgeiss0702 <aj@ajg0702.us>
2024-06-03 13:24:04 -0700

    redirect misspellings

--- src/routes/project/[project]/+page.server.ts
+++ src/routes/project/[project]/+page.server.ts
@@ -1,5 +1,5 @@
 import {projects} from "$lib/projects";
-import {error, type LoadEvent} from "@sveltejs/kit";
+import {error, type LoadEvent, redirect} from "@sveltejs/kit";
 
 export function load({ params }: LoadEvent) {
     const projectName = params.project;
@@ -7,6 +7,8 @@ export function load({ params }: LoadEvent) {
     for (const prj of projects) {
         if(prj.name === projectName) {
             return prj;
+        } else if(prj.name.toLowerCase() === projectName?.toLowerCase()) {
+            throw redirect(302, `/project/${prj.name}/`);
         }
     }
 
